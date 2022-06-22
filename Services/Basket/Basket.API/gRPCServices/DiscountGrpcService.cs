38a08dfa2f91a2d0412c9ee096815e2a06a7402b
TmMaybeCan <timurxb3@gmail.com>
2022-06-22 17:52:31 +0300

    Added DiscountGrpcService with basic logic

---
+++ Services/Basket/Basket.API/gRPCServices/DiscountGrpcService.cs
@@ -0,0 +1,25 @@
+﻿using Discount.gRPC.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace Basket.API.gRPCServices
+{
+    public class DiscountGrpcService
+    {
+        readonly private DiscountProtoService.DiscountProtoServiceClient _discountProtoServiceClient;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoServiceClient)
+        {
+            _discountProtoServiceClient = discountProtoServiceClient ?? throw new ArgumentNullException(nameof(discountProtoServiceClient));
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName =  productName};
+
+            var result = await _discountProtoServiceClient.GetDiscountAsync(discountRequest);
+
+            return result;
+        }
+    }
+}
